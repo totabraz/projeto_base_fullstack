77d4d5084d4f66b225971a734cbdd5b333067feb
TotaBraz <tota.braz@gmail.com>
2019-11-11 15:49:13 -0300

    update

--- application/controllers/admin/PortalConfigs.php
+++ application/controllers/admin/PortalConfigs.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class PortalConfigs extends CI_Controller
+class Portalconfigs extends CI_Controller
 {
 
     function __construct()
